559027b7a66df2757366bda1916484f052fded94
Danielle Arquisola <64483418+daniellearquisola@users.noreply.github.com>
2020-09-23 13:53:11 -0500

    chore: added thunk creator

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -8,3 +8,13 @@ export const setSingleProduct = (product) => ({
   type: SET_SINGLE_PRODUCT,
   product,
 })
+
+//THUNK CREATOR
+export const fetchSingleProduct = (id) => async (dispatch) => {
+  try {
+    const {data: oneProduct} = await axios.get(`/api/products/${id}`)
+    dispatch(setSingleProduct(oneProduct))
+  } catch (error) {
+    console.error(error)
+  }
+}
