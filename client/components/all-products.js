046cff730a6ab9d61719c8133b1aa527aa75dd97
David Moon <moon.kyoungje@gmail.com>
2020-09-23 12:57:45 -0400

    feat: connect all products to single product

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -3,11 +3,27 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {AllProductsCard} from './all-products-card'
 
+const dummyData = [
+  {
+    id: 1,
+    name: 'beans',
+    price: '$1',
+    imageUrl: 'https://i.imgur.com/DLrwUP7.png',
+  },
+  {
+    id: 2,
+    name: 'a wand',
+    price: '$10',
+    imageUrl: 'https://i.imgur.com/jim3MSJ.png',
+  },
+]
+
 /**
  * COMPONENT
  */
 export const AllProducts = (props) => {
-  const {products} = props
+  //const {products} = props
+  const products = props.products || dummyData
 
   return (
     <div>
