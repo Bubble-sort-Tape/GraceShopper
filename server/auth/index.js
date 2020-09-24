39e9dd873d647b918b07c4cc4722fd80dbf8bb71
David Moon <moon.kyoungje@gmail.com>
2020-09-24 13:51:14 -0400

    feat: secure login and create account routes

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,6 +2,30 @@ const router = require('express').Router()
 const User = require('../db/models/user')
 module.exports = router
 
+function filterKeys(allowedKeys, dirtyObject) {
+  const cleanObject = {}
+  for (const key of allowedKeys) {
+    if (key in dirtyObject) {
+      cleanObject[key] = dirtyObject[key]
+    }
+  }
+  return cleanObject
+}
+
+//Cleans a fetched user to send to the client
+function cleanUser(user) {
+  const allowKeys = [
+    'id',
+    'email',
+    'firstName',
+    'lastName',
+    'house',
+    'phone',
+    'isAdmin',
+  ]
+  return filterKeys(allowKeys, user)
+}
+
 router.post('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({where: {email: req.body.email}})
@@ -12,7 +36,10 @@ router.post('/login', async (req, res, next) => {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
     } else {
-      req.login(user, err => (err ? next(err) : res.json(user)))
+      //after checking password, clean user before returning data
+      const cleanedUser = cleanUser(user)
+
+      req.login(user, (err) => (err ? next(err) : res.json(cleanedUser)))
     }
   } catch (err) {
     next(err)
@@ -21,8 +48,21 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
-    req.login(user, err => (err ? next(err) : res.json(user)))
+    const user = await User.create(req.body, {
+      fields: [
+        'email',
+        'firstName',
+        'lastName',
+        'house',
+        'phone',
+        'password',
+        'salt',
+      ],
+    })
+
+    //after creating user, clean user instance before returning data
+    const cleanedUser = cleanUser(user)
+    req.login(user, (err) => (err ? next(err) : res.json(cleanedUser)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
@@ -39,7 +79,13 @@ router.post('/logout', (req, res) => {
 })
 
 router.get('/me', (req, res) => {
-  res.json(req.user)
+  if (!req.user) {
+    res.status(404).send('Not logged in')
+  } else {
+    //after creating user, clean user instance before returning data
+    const cleanedUser = cleanUser(req.user)
+    res.json(cleanedUser)
+  }
 })
 
 router.use('/google', require('./google'))
