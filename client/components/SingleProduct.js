f09b9dcf23282b174b3216145dd3af7ec6fa4046
David Moon <moon.kyoungje@gmail.com>
2020-09-25 14:58:25 -0400

    fix: single product component

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -20,67 +20,61 @@ const dummyData = [
 
 export class SingleProduct extends React.Component {
   constructor(props) {
-    super(props)
+    super()
     this.state = {
       product: {},
     }
   }
 
-  const {product} = props || dummyData
-  console.log('\n\n\nprops\n\n\n', props)
-  // let {productId} = props.match.params
-  // let products = props.products
-  // let product = products.find((obj) => {
-  //   return obj.id === Number(productId)
-  // })
+  componentDidMount() {
+    this.props.fetchSingleProduct(this.props.match.params.productId)
+  }
 
-  return (
-    <div className="Product">
-      <div className="Product-left">
-        <img className="Product-image" src={product.imageUrl} />
-        <div>{product.name}</div>
-        <div>Price: ${product.price}</div>
-      </div>
-      <div className="Product-right">
-        <div>About this product: {product.description}</div>
-        <div>
-          <form>
-            <label>
-              Quantity
-              <select>
-                <option selected value="one">
-                  1
-                </option>
-                <option value="two">2</option>
-                <option value="three">3</option>
-                <option value="four">4</option>
-                <option value="five">5</option>
-                <option value="six">6</option>
-                <option value="seven">7</option>
-                <option value="eight">8</option>
-                <option value="nine">9</option>
-                <option value="ten">10</option>
-              </select>
-            </label>
-            <input type="submit" value="Checkout" />
-          </form>
+  render() {
+    const {product} = this.props || dummyData
+    console.log('\n\n\nprops\n\n\n', this.props)
+    // let {productId} = props.match.params
+    // let products = props.products
+    // let product = products.find((obj) => {
+    //   return obj.id === Number(productId)
+    // })
+
+    return (
+      <div className="Product">
+        <div className="Product-left">
+          <img className="Product-image" src={product.imageUrl} />
+          <div>{product.name}</div>
+          <div>Price: ${product.price}</div>
+        </div>
+        <div className="Product-right">
+          <div>About this product: {product.description}</div>
+          <div>
+            <form>
+              <label>
+                Quantity
+                <select>
+                  <option selected value="one">
+                    1
+                  </option>
+                  <option value="two">2</option>
+                  <option value="three">3</option>
+                  <option value="four">4</option>
+                  <option value="five">5</option>
+                  <option value="six">6</option>
+                  <option value="seven">7</option>
+                  <option value="eight">8</option>
+                  <option value="nine">9</option>
+                  <option value="ten">10</option>
+                </select>
+              </label>
+              <input type="submit" value="Checkout" />
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-const mapState = (state) => ({
-  product: state.singleProduct,
-})
-
-const mapDispatch = (dispatch) => ({
-  fetchSingleProduct: () => {
-    dispatch(fetchSingleProduct())
-  },
-})
-
-export default connect(mapState, mapDispatch)(SingleProduct)
 
 SingleProduct.propTypes = {
   products: PropTypes.object,
@@ -91,8 +85,8 @@ const mapState = (state) => ({
 })
 
 const mapDispatch = (dispatch) => ({
-  fetchSingleProduct: () => {
-    dispatch(fetchSingleProduct())
+  fetchSingleProduct: (id) => {
+    dispatch(fetchSingleProduct(id))
   },
 })
 
