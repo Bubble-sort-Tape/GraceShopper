228d5b194aada82bf8f071b61332ed602d7ac4ca
David Moon <moon.kyoungje@gmail.com>
2020-09-27 13:39:01 -0400

    test: ensure addcart axios call sends the correct information

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,34 +1,71 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {fetchCartItems} from './cart'
+import {fetchCartItems, addCartItem, editCartItem, removeCartItem} from './cart'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
 import thunkMiddleware from 'redux-thunk'
+import {fake} from 'faker'
 
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
-describe('AllProducts Redux', () => {
+describe('Cart Redux', () => {
   let store
   let mockAxios
 
   const initialCart = {
     items: [],
   }
+
+  const fakeProductsList = [
+    {
+      id: 1,
+      name: 'beans',
+      price: 200,
+      imageUrl: 'https://i.imgur.com/DLrwUP7.png',
+    },
+    {
+      id: 2,
+      name: 'a wand',
+      price: 250,
+      imageUrl: 'https://i.imgur.com/jim3MSJ.png',
+    },
+    {
+      id: 3,
+      name: 'frog',
+      price: 330,
+      imageUrl: 'https://i.imgur.com/jim3MSJ.png',
+    },
+    {
+      id: 4,
+      name: 'one muggle dollar',
+      price: 150,
+      imageUrl: 'https://i.imgur.com/jim3MSJ.png',
+    },
+  ]
+
   const fakeCart = [
     {
       id: 1,
       name: 'beans',
       price: '$1',
       imageUrl: 'https://i.imgur.com/DLrwUP7.png',
+      OrdersProducts: {
+        quantity: 2,
+        price: 200,
+      },
     },
     {
       id: 2,
       name: 'a wand',
       price: '$10',
       imageUrl: 'https://i.imgur.com/jim3MSJ.png',
+      OrdersProducts: {
+        quantity: 1,
+        price: 250,
+      },
     },
   ]
 
@@ -51,4 +88,54 @@ describe('AllProducts Redux', () => {
       expect(actions[0].cart).to.deep.equal(fakeCart)
     })
   })
+
+  describe('addCartItem', () => {
+    it('eventually dispatches the GOT CART action', async () => {
+      mockAxios.onPost('/api/orders/cart').reply(201, fakeCart)
+      await store.dispatch(addCartItem(4, 1))
+      const actions = store.getActions()
+      expect(actions[0].type).to.equal('GOT_CART')
+    })
+
+    it('returns an updated cart', async () => {
+      mockAxios.onPost('/api/orders/cart').reply(function (config) {
+        const {id, quantity} = JSON.parse(config.data)
+
+        const newProd = fakeProductsList.find((prod) => {
+          return prod.id === id
+        })
+
+        newProd.OrdersProducts = {quantity, price: newProd.price}
+        fakeCart.push(newProd)
+
+        return [201, fakeCart]
+      })
+      await store.dispatch(addCartItem(1, 1))
+      const actions = store.getActions()
+      expect(actions[0].type).to.equal('GOT_CART')
+      expect(actions[0].cart[2].id).to.equal(4)
+      expect(actions[0].cart[2].quantity).to.equal(1)
+    })
+  })
+
+  /*
+  describe('editCartItem', () => {
+    it('eventually dispatches the GOT CART action', async () => {
+      mockAxios.onPost('/api/orders/cart').replyOnce(200, fakeCart)
+      await store.dispatch(editCartItem())
+      const actions = store.getActions()
+      expect(actions[0].type).to.equal('GOT_CART')
+      expect(actions[0].cart).to.deep.equal(fakeCart)
+    })
+  })
+  describe('removeCartItems', () => {
+    it('eventually dispatches the GOT CART action', async () => {
+      mockAxios.onPost('/api/orders/cart').replyOnce(200, fakeCart)
+      await store.dispatch(removeCartItem())
+      const actions = store.getActions()
+      expect(actions[0].type).to.equal('GOT_CART')
+      expect(actions[0].cart).to.deep.equal(fakeCart)
+    })
+  })
+   */
 })
